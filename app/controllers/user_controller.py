5ca8932dfef8715b373a1e4c42dbc3d470bb1d53
dszklarz <david.szklarzewski@gmail.com>
2022-07-26 17:01:18 +0200

    save for the day

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,6 +1,6 @@
 from app import app
 
-from flask import render_template
+from flask import render_template, session
 
 from app.services.user_service import UserService
 
@@ -8,5 +8,9 @@ userservice = UserService()
 
 @app.route('/user/<int:userid>')
 def profile_page(userid: int):
-    return render_template('user/user.html',user= userservice.find_one(userid))
+    return render_template('user/user.html', user= userservice.find_one(userid))
 
+@app.route('/login')
+def login_page():
+    print(session.get('username'))
+    return render_template('user/login.html')
