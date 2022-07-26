68767b6d28c061eb990b385f6c358daf7bc31f1d
CrenierAmaury <amaurycrenier@gmail.com>
2022-07-26 14:40:49 +0200

    add nullable to model and migration

--- app/models/comment.py
+++ app/models/comment.py
@@ -4,7 +4,7 @@ from app.models.base_entity import BaseEntity
 class Comment(db.Model, BaseEntity):
     __tablename__ = "comments"
     comment_id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.ForeignKey('users.user_id'))
-    user_service_id = db.Column(db.ForeignKey('users_services.user_service_id'))
-    service_id = db.Column(db.ForeignKey('services.service_id'))
+    author_id = db.Column(db.ForeignKey('users.user_id'), nullable=False)
+    user_service_id = db.Column(db.ForeignKey('user_services.user_service_id'), nullable=True)
+    service_id = db.Column(db.ForeignKey('services.service_id'), nullable=True)
     content = db.Column(db.String(500), nullable=False)
