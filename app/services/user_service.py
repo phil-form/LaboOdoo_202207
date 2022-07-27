e504c3370ff214d6535bb6ef26258ecb3cdf8b99
dszklarz <david.szklarzewski@gmail.com>
2022-07-27 14:55:07 +0200

    basic form validation + dtos / services

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,6 +1,8 @@
 from app.services.base_service import BaseService
 from app.models.user import User
 from app.dtos.user_dto import UserDTO
+from app import db
+from bcrypt import gensalt, hashpw
 
 
 class UserService(BaseService):
@@ -13,8 +15,12 @@ class UserService(BaseService):
     def find_one_by(self, **kwargs):
         pass
 
-    def insert(self, data):
-        pass
+    def insert(self, data: UserDTO):
+        print(data.password)
+        user = data.dto_to_entity()
+        encrypted_pass = hashpw(user.password.encode('utf-8'), gensalt()).decode('utf-8')
+        user.password = encrypted_pass
+        print(user.password)
 
     def update(self, entity_id: int, data):
         pass
